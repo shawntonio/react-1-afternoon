31fc68b9e9436feea50c0089facb2ce82173093a
Shawn Turley <antonioturley@gmail.com>
2019-04-01 16:09:11 -0600

    finished filter object problem

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,51 @@
 import React, {Component} from 'react';
 
 export default class FilterObject extends Component {
+	constructor(){
+		super()
+		this.state = {
+			unFilteredArray: [
+        {
+          name: 'Jimmy Joe',
+          title: 'Hack0r',
+          age: 12,
+        },
+        {
+          name: 'Jeremy Schrader',
+          age: 24,
+          hairColor: 'brown'
+        },
+        {
+          name: 'Carly Armstrong',
+          title: 'CEO',
+        }
+      ],
+			userInput: "",
+			filteredArray: []
+		}
+	}
+
+	inputHandler = (e) => {
+		this.setState({
+			userInput: e.target.value
+		})
+	}
+
+	filterArray = () => { 
+		this.setState({
+			filteredArray: this.state.unFilteredArray.filter(obj => obj[this.state.userInput])
+		})
+	}
+	
 	render(){
 		return (
-			<p>Filter Object</p>
+			<div className="puzzleBox filterObjectPB">
+				<h4>Filter Object</h4>
+				<span className="puzzleText">Original: {JSON.stringify(this.state.unFilteredArray)}</span>
+				<input onChange={this.inputHandler} className="inputLine"></input>
+				<button onClick={this.filterArray} className="confirmationButton">Filter</button>
+				<span className="resultsBox filterObjectRB">Filtered: {JSON.stringify(this.state.filteredArray)}</span>
+			</div>
 		);
 	}
 	
