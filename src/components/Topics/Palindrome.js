989588da19c350b1ed4c69778f4a74330b5519d9
Shawn Turley <antonioturley@gmail.com>
2019-04-01 19:46:22 -0600

    finsished sum problem

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,42 @@
 import React, {Component} from 'react';
 
 export default class Palindrome extends Component {
+	constructor(){
+		super()
+
+		this.state = {
+			userInput: "",
+			palindrome: ""
+		}
+	}
+	
+	inputHandler = (e) => {
+		this.setState({
+			userInput: e.target.value.toLowerCase()
+		})
+	}
+
+	check = () => {
+		let str = this.state.userInput;
+		let pal = "";
+		if (str === str.split('').reverse().join(""))   pal = "true" 
+			else  pal = "false"
+		
+		this.setState({
+			palindrome: pal
+		})
+		console.log(this.state.palindrome)
+	}
+	
 	render(){
 		return (
-			<p>Palindrome</p>
+			<div className="puzzleBox filterStringPB">
+				<h4>Palindrome</h4>
+				<input onChange={this.inputHandler} className="inputLine"></input>
+				<button onClick={this.check} className="confirmationButton">Check</button>
+				<span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+
+			</div>
 		);
 	}
 	
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react';
 
 export default class Sum extends Component {
+	constructor(){
+		super()
+
+		this.state = {
+			number1: 0,
+			number2: 0,
+			sum: null
+		}
+	}
+	
+	input1 = (e) => {
+		this.setState({
+			number1: parseInt(e.target.value)
+		})
+	}
+	input2 = (e) => {
+		this.setState({
+			number2: parseInt(e.target.value)
+		})
+	}
+	addNums = () => {
+		this.setState({
+			sum: this.state.number1 + this.state.number2
+		})
+	}
+	
 	render(){
 		return (
-			<p>Sum</p>
+			<div className="puzzleBox sumPB">
+				<h4>Sum</h4>
+				<input onChange={this.input1} className="inputLine"></input>
+				<input onChange={this.input2} className="inputLine"></input>
+				<button onClick={this.addNums} className="confirmationButton">Add</button>
+				<span className="resultsBox">Sum: {this.state.sum}</span>
+			</div>
 		);
 	}
 	
